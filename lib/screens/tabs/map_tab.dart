340fb1bfcd1764924b5d6ed1b7c15df814c4dd05
Lans12345 <olanalans12345@gmail.com>
2023-02-06 07:23:37 +0800

    added maps tab

--- lib/screens/tabs/map_tab.dart
+++ lib/screens/tabs/map_tab.dart
@@ -1,1 +1,43 @@
+import 'dart:async';
 
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class MapTab extends StatefulWidget {
+  const MapTab({Key? key}) : super(key: key);
+
+  @override
+  State<MapTab> createState() => MapTabState();
+}
+
+class MapTabState extends State<MapTab> {
+  final Completer<GoogleMapController> _controller =
+      Completer<GoogleMapController>();
+
+  static const CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(8.308721, 124.944031),
+    zoom: 14.4746,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GoogleMap(
+        circles: {
+          Circle(
+            circleId: const CircleId("circle1"),
+            center: const LatLng(8.348975, 124.972012),
+            radius: 100,
+            strokeColor: Colors.blue,
+            fillColor: Colors.blue.withOpacity(0.5),
+          ),
+        },
+        mapType: MapType.normal,
+        initialCameraPosition: _kGooglePlex,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+      ),
+    );
+  }
+}
