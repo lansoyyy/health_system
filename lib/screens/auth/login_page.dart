1c347e22cff8ea6eca1ef4d235140f50049b2e08
Lans12345 <olanalans12345@gmail.com>
2023-04-09 14:16:33 +0800

    added config for login page

--- lib/screens/auth/login_page.dart
+++ lib/screens/auth/login_page.dart
@@ -9,6 +9,8 @@ import 'package:sumilao/widgets/textfield_widget.dart';
 import 'package:sumilao/widgets/toast_widget.dart';
 
 class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
   @override
   State<LoginPage> createState() => _LoginPageState();
 }
@@ -22,104 +24,125 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      body: Column(
         children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              TextBold(text: '  GeoFinds', fontSize: 75, color: primary),
-              Container(
-                height: 350,
-                width: 600,
-                decoration: const BoxDecoration(
-                  image: DecorationImage(
-                      image: AssetImage('assets/images/background.png'),
-                      fit: BoxFit.fitWidth),
-                ),
-              ),
-            ],
+          const SizedBox(
+            height: 75,
           ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.center,
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              TextBold(text: 'LOGIN', fontSize: 24, color: Colors.black),
-              TextRegular(
-                  text: 'Welcome back! Please enter your details',
-                  fontSize: 16,
-                  color: Colors.black),
-              const SizedBox(
-                height: 20,
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextBold(text: '  GeoFinds', fontSize: 75, color: primary),
+                  Container(
+                    height: 350,
+                    width: 600,
+                    decoration: const BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage('assets/images/background.png'),
+                          fit: BoxFit.fitWidth),
+                    ),
+                  ),
+                ],
               ),
-              TextFieldWidget(
-                  label: 'Username or Email: ', controller: emailController),
-              const SizedBox(
-                height: 20,
-              ),
-              TextFieldWidget(
-                  inPassword: true,
-                  isObscure: true,
-                  label: 'Password: ',
-                  controller: passwordController),
-              // TextButton(
-              //     onPressed: (() {}),
-              //     child: TextBold(
-              //         text: 'Forgot Password?',
-              //         fontSize: 12,
-              //         color: Colors.black)),
-              const SizedBox(
-                height: 30,
-              ),
-              Center(
-                child: ButtonWidget(
-                    label: 'Login',
-                    onPressed: (() async {
-                      if (emailController.text == 'admin-username' &&
-                          passwordController.text == 'admin-password') {
-                        box.write('user', 'Admin');
-                        Navigator.pushReplacementNamed(context, '/homescreen');
-                      } else {
-                        late var isDeleted;
-                        late var role;
-                        try {
-                          var collection = FirebaseFirestore.instance
-                              .collection('User')
-                              .where('email', isEqualTo: emailController.text);
-
-                          var querySnapshot = await collection.get();
-                          var user = await FirebaseAuth.instance
-                              .signInWithEmailAndPassword(
-                                  email: emailController.text,
-                                  password: passwordController.text);
-
-                          setState(() {
-                            for (var queryDocumentSnapshot
-                                in querySnapshot.docs) {
-                              Map<String, dynamic> data =
-                                  queryDocumentSnapshot.data();
-                              isDeleted = data['isDeleted'];
-                              role = data['role'];
-                            }
-                          });
-
-                          if (isDeleted == true) {
-                            showToast('Your account has been deleted!');
-                            await FirebaseAuth.instance.signOut();
-                          } else {
-                            box.write('user', role);
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextBold(text: 'LOGIN', fontSize: 24, color: Colors.black),
+                  TextRegular(
+                      text: 'Welcome back! Please enter your details',
+                      fontSize: 16,
+                      color: Colors.black),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  TextFieldWidget(
+                      label: 'Username or Email: ',
+                      controller: emailController),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  TextFieldWidget(
+                      inPassword: true,
+                      isObscure: true,
+                      label: 'Password: ',
+                      controller: passwordController),
+                  // TextButton(
+                  //     onPressed: (() {}),
+                  //     child: TextBold(
+                  //         text: 'Forgot Password?',
+                  //         fontSize: 12,
+                  //         color: Colors.black)),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  Center(
+                    child: ButtonWidget(
+                        label: 'Login',
+                        onPressed: (() async {
+                          if (emailController.text == 'admin-username' &&
+                              passwordController.text == 'admin-password') {
+                            box.write('user', 'Admin');
                             Navigator.pushReplacementNamed(
                                 context, '/homescreen');
+                          } else {
+                            late var isDeleted;
+                            late var role;
+                            try {
+                              var collection = FirebaseFirestore.instance
+                                  .collection('User')
+                                  .where('email',
+                                      isEqualTo: emailController.text);
+
+                              var querySnapshot = await collection.get();
+                              var user = await FirebaseAuth.instance
+                                  .signInWithEmailAndPassword(
+                                      email: emailController.text,
+                                      password: passwordController.text);
+
+                              setState(() {
+                                for (var queryDocumentSnapshot
+                                    in querySnapshot.docs) {
+                                  Map<String, dynamic> data =
+                                      queryDocumentSnapshot.data();
+                                  isDeleted = data['isDeleted'];
+                                  role = data['role'];
+                                }
+                              });
+
+                              if (isDeleted == true) {
+                                showToast('Your account has been deleted!');
+                                await FirebaseAuth.instance.signOut();
+                              } else {
+                                box.write('user', role);
+                                Navigator.pushReplacementNamed(
+                                    context, '/homescreen');
+                              }
+                            } catch (e) {
+                              showToast(e.toString());
+                            }
                           }
-                        } catch (e) {
-                          showToast(e.toString());
-                        }
-                      }
-                    })),
-              )
+                        })),
+                  )
+                ],
+              ),
             ],
           ),
+          const Expanded(child: SizedBox()),
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.only(left: 400, right: 400, bottom: 20),
+              child: TextRegular(
+                  text:
+                      'Republic Act No. 10173, otherwise known as the Data Privacy Act is a law that seeks to protect all forms of information, be it private, personal, or sensitive. It is meant to cover both natural and juridical persons involved in the processing of personal information.',
+                  fontSize: 12,
+                  color: Colors.grey),
+            ),
+          )
         ],
       ),
     );
